9bd8403d7ce036e11bc0e0d013e260d071494a89
tungdt <duongthanhtungvn01@gmail.com>
2020-04-07 00:44:34 +0700

    Add files

---
+++ main.py
@@ -0,0 +1,11 @@
+from scipy.io import wavfile
+from tensorflow.contrib import predictor
+
+input_file = "audio_example.wav"
+waveform = wavfile.read(input_file)[1]
+waveform = waveform/32768.0
+
+model = predictor.from_saved_model('model')
+result = model({'waveform': waveform, 'audio_id': 'audio_example.wav'})
+wavfile.write('vocals.wav', 44100, result['vocals'])
+wavfile.write('accompaniment.wav', 44100, result['accompaniment'])
